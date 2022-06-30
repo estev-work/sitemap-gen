309ff5f7ef10150fb1db658cf262a834af7b7980
estev-work <estev.work@gmail.com>
2022-06-30 13:11:53 +0700

    add validate from input param

---
+++ src/Util/Validate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Estev\Sitemap\Util;
+
+use DateTime;
+use Estev\Sitemap\Element;
+use Estev\Sitemap\Enums\ChangeFreq;
+use Estev\Sitemap\Exceptions\CreateSiteMapElementException;
+
+class Validate
+{
+    /**
+     * @param array $pageParam ["loc"=>"https://site.ru/", "lastmod"=>"2020-12-14", "priority"=>1, "changefreq"=>"hourly"]
+     * @throws CreateSiteMapElementException
+     * @throws \Exception
+     */
+    public static function validateElementParam(array &$pageParam): void
+    {
+        self::checkEmpty($pageParam, 'loc');
+        self::checkEmpty($pageParam, 'lastmod');
+        self::checkEmpty($pageParam, 'priority');
+        self::checkEmpty($pageParam, 'changefreq');
+
+        $dateData = date_parse($pageParam['lastmod']);
+        if (count($dateData['errors'])) {
+            throw new CreateSiteMapElementException('"lastmod" date not valid');
+        }
+
+        if (!is_numeric($pageParam['priority'])) {
+            throw new CreateSiteMapElementException('"lastmod" not numeric');
+        }
+    }
+
+    /**
+     * @throws CreateSiteMapElementException
+     */
+    private static function checkEmpty(array $pageParams, string $paramName): void
+    {
+        if (empty($pageParams[$paramName])) {
+            throw new CreateSiteMapElementException('"'.$paramName.'" empty');
+        }
+    }
+}
